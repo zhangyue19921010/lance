35f416b97b7f7a6060f1511ca6fb87d4a260bc96
zhangyue19921010 <zhangyue.1010@bytedance.com>
2026-05-06 15:55:08 +0800

    fix ut

--- rust/lance/src/dataset.rs
+++ rust/lance/src/dataset.rs
@@ -1865,7 +1865,7 @@ impl Dataset {
     ) -> Result<(Arc<ObjectStore>, Path)> {
         let object_path = self
             .data_file_dir(data_file)?
-            .child(data_file.path.as_str());
+            .join(data_file.path.as_str());
         let object_store = self.object_store(data_file.base_id).await?;
         Ok((object_store, object_path))
     }
